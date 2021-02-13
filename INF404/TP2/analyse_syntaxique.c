e8c5315f285d3e8ff60111506f7e666ec423550d
collomsu <39761559+collomsu@users.noreply.github.com>
2021-02-13 12:19:54 +0100

    Ajout evaluer + modification fonction Rec (manque a passer les paramettre en *)
    Manque a passer les paramètres de s fonctions rec en pointeurs pour prendre ne compte les modification local des fonctions + ajouter le calcul du résultat total

--- INF404/TP2/analyse_syntaxique.c
+++ INF404/TP2/analyse_syntaxique.c
@@ -7,18 +7,39 @@
 #include "lecture_caracteres.h"
 #include "analyse_lexicale.h"
 
-/* etat initial : indifferent
-etat final : une EAEP a ete lue dans le nom_fichier
-si elle ne contient pas d'erreur de syntaxe un message est affiche
-sinon le pgm termine sur un message d'erreur */
+float evaluer(float vald, float valg, char op){
+  switch (op) {
+    case '+':
+      return (float)(valg+vald);
+      break;
+    case '-':
+      return (float)(valg-vald);
+      break;
+    case '*':
+      return (float)(valg*vald);
+      break;
+    case '/':
+      if(vald == 0){
+        printf("ERREUR : Division par 0\n");
+        return (float)(0.000001);
+      } else {
+        return (float)(valg/vald);
+      }
+      break;
+    default:
+      printf("ERREUR : Operateur inconnu\n");
+      return (float)(0.000001);
+  }
+}
 
-bool Rec_op(Lexeme *lex){
+bool Rec_op(Lexeme *lex, char op){
   printf("%s\n",lex->chaine);
   switch (lex->nature) {
     case PLUS:
     case MUL:
     case MOINS:
     case DIV:
+      op = *lex->chaine;
       avancer();
       *lex = lexeme_courant();
       break;
@@ -29,19 +50,27 @@ bool Rec_op(Lexeme *lex){
   return 1;
 }
 
-bool Rec_eaep(Lexeme *lex){
+bool Rec_eaep(Lexeme *lex, float val){
   printf("%s\n",lex->chaine);
+  float valg = 0;
+  float vald = 0;
+  char op = '\0';
   switch (lex->nature) {
     case ENTIER:
       avancer();
       *lex = lexeme_courant();
+      val = lex->valeur;
       break;
     case PARO:
       avancer();
       *lex = lexeme_courant();
-      Rec_eaep(lex);
-      Rec_op(lex);
-      Rec_eaep(lex);
+      Rec_eaep(lex, valg);
+      Rec_op(lex, op);
+      Rec_eaep(lex, vald);
+      val = evaluer(valg,vald,op);
+      if(val == (float)(0.000001)){
+        return 0;
+      }
       if(lex->nature == PARF){
         printf("%s\n",lex->chaine);
         avancer();
@@ -58,21 +87,29 @@ bool Rec_eaep(Lexeme *lex){
   return 1;
 }
 
+/* etat initial : indifferent
+etat final : une EAEP a ete lue dans le nom_fichier
+si elle ne contient pas d'erreur de syntaxe un message est affiche
+et la valeur de cette expression est fournie dans le parametre resultat
+sinon le pgm termine sur un message d'erreur */
+
 void analyser(char *fichier, float *resultat){
   Lexeme *lex = malloc(sizeof(Lexeme));
 
   *resultat = 0;
+  float val = 0;
+  char op = '\0';
 
   demarrer(fichier);
   *lex = lexeme_courant();
   while(! fin_de_sequence()){
-    if(!Rec_eaep(lex)){
+    if(!Rec_eaep(lex, val)){
       printf("ERREUR : Syntaxe incorrect.\n");
       *resultat = 0.000001;
       break;
     } else {
       if(! fin_de_sequence()){
-        if(!Rec_op(lex)){
+        if(!Rec_op(lex, op)){
           printf("ERREUR : Syntaxe incorrect.\n");
           *resultat = 0.000001;
           break;
--- INF404/TP2/analyse_syntaxique.h
+++ INF404/TP2/analyse_syntaxique.h
@@ -3,8 +3,9 @@
 
 void analyser(char *fichier, float *resultat);
 /* etat initial : indifferent
-etat final : une Expression Arithmetique a ete lue dans le fichier
-si ell ne contient pas d'erreur de syntaxe un message est affiche
-sinon le pgm termine sur un message d'erreur*/
+etat final : une EAEP a ete lue dans le nom_fichier
+si elle ne contient pas d'erreur de syntaxe un message est affiche
+et la valeur de cette expression est fournie dans le parametre resultat
+sinon le pgm termine sur un message d'erreur */
 
 #endif
