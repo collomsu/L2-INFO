5c858785949eeabcede21e23cebbe83f4bfb0a8c
BuildTools <unconfigured@null.spigotmc.org>
2021-03-02 14:47:34 +0100

    add projet STAT (à finir)

---
+++ STA401/TP/Projet/RScriptProjet.R
@@ -0,0 +1,29 @@
+#Création des variables avec les données récoltées (en min)
+res <- Sondage.sur.le.temps.de.repas
+midi <- res$Midi
+soir <- res$Soir
+
+#Summary des résultats
+summary(res)
+
+#Ecart-type pour midi et soir
+sd(midi)
+sd(soir)
+
+#Graphique
+
+#boxplot
+boxplot(midi,soir, main="Temps de repas le midi et le soir (en min)", ylab="Temps (en min)", names=c("Midi","Soir"))
+
+#histogramme
+par(mfrow=c(1,2))
+plot(hist(midi, main=""))
+curve(dnorm(x,mean(midi),sd(midi)))
+hist(soir, main="")
+
+#quantile-quantile plot
+par(mfrow=c(1,2))
+qqnorm(midi, main="histogramme des résultats du midi")
+qqline(midi)
+qqnorm(soir, main="histogramme des résultats du soir")
+qqline(soir)
